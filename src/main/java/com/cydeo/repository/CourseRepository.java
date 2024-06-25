56add0becc2a41b56e5a91b548d6697c6ba88b7e
enesorhan <enestcp@gmail.com>
2024-06-25 15:49:30 +0300

    CourseRepository

--- src/main/java/com/cydeo/repository/CourseRepository.java
+++ src/main/java/com/cydeo/repository/CourseRepository.java
@@ -11,4 +11,8 @@ public interface CourseRepository extends JpaRepository<Course,Long> {
     List<Course> findByCategory(String category);
 
     //find all courses by categories and order the entities by name
+    List<Course> findByCategoryOrderByName(String category);
+
+    //checks if a course with the supplied name exists Return true if exists, false if not
+    boolean existsByName(String name);
 }
